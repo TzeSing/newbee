b84ddd33d4b0d60923760da8b7cf374570a0a8e9
zixing.jiang <zixing.jiang@klook.com>
2023-05-03 22:27:04 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Index from '@/views/Index.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: Index
+    }
+  ]
+})
+
+export default router
